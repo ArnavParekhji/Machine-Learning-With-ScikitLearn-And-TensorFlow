66341b911022aa5d8657ebd2e75cc25f77605306
Arnav Parekhji <arnav17121999@gmail.com>
2019-03-09 15:59:35 +0530

    Data Visualization and Data Cleaning

--- HousingPricePrediction/housingPrediction.py
+++ HousingPricePrediction/housingPrediction.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+import matplotlib.image as mpimg
+from pandas.tools.plotting import scatter_matrix
 
 style.use('ggplot')
 
@@ -19,4 +21,61 @@ for train_index, test_index in split.split(housing, housing['income_cat']):
 
 for set_ in (strat_train_set, strat_test_set):
     set_.drop("income_cat", axis=1, inplace=True)
+
+housing = strat_train_set.copy()
+
+#--------------------TO SAVE SCATTER PLOT OVER CALIFORNIA MAP--------------------
+california_img=mpimg.imread('california.png')
+ax = housing.plot(kind="scatter", x="longitude", y="latitude", figsize=(10,7),
+                       s=housing['population']/100, label="Population",
+                       c="median_house_value", cmap=plt.get_cmap("jet"),
+                       colorbar=False, alpha=0.4,
+                      )
+plt.imshow(california_img, extent=[-124.55, -113.80, 32.45, 42.05], alpha=0.5,
+           cmap=plt.get_cmap("jet"))
+plt.ylabel("Latitude", fontsize=14)
+plt.xlabel("Longitude", fontsize=14)
+
+prices = housing["median_house_value"]
+tick_values = np.linspace(prices.min(), prices.max(), 11)
+cbar = plt.colorbar()
+cbar.ax.set_yticklabels(["$%dk"%(round(v/1000)) for v in tick_values], fontsize=14)
+cbar.set_label('Median House Value', fontsize=16)
+
+plt.legend(fontsize=16)
+plt.savefig("california_housing_prices_plot")
+#--------------------****************************************--------------------
+
+housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
+housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
+housing["population_per_household"]=housing["population"]/housing["households"]
+
+corr_matrix = housing.corr()
+
+
+#--------------------DATA CLEANING--------------------
+housing.drop("median_house_value", axis=1, inplace=True)
+housing_labels = strat_train_set["median_house_value"].copy()
+
+try:
+    from sklearn.impute import SimpleImputer 
+except ImportError:
+    from sklearn.preprocessing import Imputer as SimpleImputer
+
+imputer = SimpleImputer(strategy='median')
+
+housing_num = housing.drop("ocean_proximity", axis=1)
+imputer.fit(housing_num)
+
+X = imputer.transform(housing_num)
+housing_tr = pd.DataFrame(X, columns=housing_num.columns)
+
+
+
+
+
+
+
+
+
 
