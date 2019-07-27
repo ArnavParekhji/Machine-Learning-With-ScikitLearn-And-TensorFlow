7fda8b1209b4ef96d4d706321b9580355cc9c7b3
Arnav Parekhji <arnav17121999@gmail.com>
2019-07-27 17:23:20 +0530

    Measuring accuracy using cross validation

--- MNISTClassification/classification.py
+++ MNISTClassification/classification.py
@@ -10,11 +10,16 @@ mnist = pickle.load(f)
 
 X, y = mnist["data"], mnist["target"]
 
+
+##-------VIEW AN IMAGE IN THE DATASET-------
+
 some_digit = X[36000]
 some_digit_image = some_digit.reshape(28, 28)
-plt.imshow(some_digit_image, cmap = matplotlib.cm.binary, interpolation="nearest")
-plt.axis("off")
-plt.show()
+##plt.imshow(some_digit_image, cmap = matplotlib.cm.binary, interpolation="nearest")
+##plt.axis("off")
+##plt.show()
+
+##------------------------------------------
 
 X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 
@@ -28,3 +33,32 @@ sgd_clf = SGDClassifier(random_state=42)
 sgd_clf.fit(X_train, y_train_5)
 
 print(sgd_clf.predict([some_digit]))
+
+##-------MEASURING ACCURACY BY IMPLEMENTING CROSS-VALIDATION-------
+
+from sklearn.model_selection import StratifiedKFold
+from sklearn.base import clone
+
+##skfolds = StratifiedKFold(n_splits=3, random_state=42)
+
+##for train_index, test_index in skfolds.split(X_train, y_train_5):
+##    clone_clf = clone(sgd_clf)
+##    X_train_folds = X_train[train_index]
+##    y_train_folds = (y_train_5[train_index])
+##    X_test_fold = X_train[test_index]
+##    y_test_fold = (y_train_5[test_index])
+##
+##    clone_clf.fit(X_train_folds, y_train_folds)
+##    y_pred = clone_clf.predict(X_test_fold)
+##    n_correct = sum(y_pred == y_test_fold)
+##    print(n_correct / len(y_pred))
+
+##-----------------------------------------------------------------
+
+##-------USING CROSS-VALIDATION-------
+
+from sklearn.model_selection import cross_val_score
+
+print(cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring="accuracy"))
+
+##------------------------------------
